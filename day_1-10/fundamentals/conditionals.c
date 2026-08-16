17071ac4a01038945077aad831753284600199d1
EmilioSA-JK <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-16 09:56:58 -0600

    variables and conditionals

---
+++ day_1-10/fundamentals/conditionals.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+    int main() {
+
+        // "<" used for "less than"
+        // ">" used for "greater than"
+        // "!=" used for "no equal to"
+        // "==" used for "equal to"
+        // "<=" used for "less than or equal to"
+        // ">=" used for "greater than or equal to"
+
+        int score = 87;
+        if(score >= 70) {
+            printf("Exam passed\n");
+        }
+
+
+        return 0;
+    }
---
+++ day_1-10/fundamentals/variables.c
@@ -0,0 +1,10 @@
+#include <stdio>
+
+int main(){
+    int integer = 67 //Whole number type(integer)
+    float floating = 45.5f; //decimal number type(floating)
+    char = 'A' //Single character type(character)
+    double = pi = 3.14159; //Double presicion floating-point
+
+    
+}
