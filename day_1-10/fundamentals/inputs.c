db79f25ba5fbef5f4e14824520ba98d9c1392723
EmilioSA-JK <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-18 16:47:33 -0600

    Inputs

---
+++ day_1-10/fundamentals/inputs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    scanf("%d", &num);
+
+    printf("you entered: %d\n", num);
+
+    
+
+
+    char favorite;
+    scanf(" %c", &favorite);
+    printf("Your favorite letter is %c\n", favorite);
+
+    return 0;
+
+}
