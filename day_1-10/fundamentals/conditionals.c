c3716c6b67a95667911c0310cd0c0cc65523f7a3
EmilioSA-JK <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-18 16:04:23 -0600

    More things added to variables

--- day_1-10/fundamentals/conditionals.c
+++ day_1-10/fundamentals/conditionals.c
@@ -13,6 +13,36 @@
             printf("Exam passed\n");
         }
 
+        else {
+            printf("Exam failed\n");
+        }
+
+        int age = 18;
+        if(age >= 18) {
+            printf("You are an adult\n");
+        }
+        // This is a nested conditional just like "elif" in python
+        else if (age >= 13){
+            printf("You're able to watch this movie with an adult\n")
+        }
+
+        else{
+
+            printf("You chiquitin, you can't watch this\n")
+
+        }
+
+
+        switch{
+
+            case
+
+
+
+
+        }
+
 
         return 0;
     }
+
--- day_1-10/fundamentals/variables.c
+++ day_1-10/fundamentals/variables.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <stdbool.h> // The stdbool is used to get boolean data type
+
 int main(){
     int integer = 67; //Whole number type(integer)
     float floating = 45.5f; //decimal number type(floating) - 7 decimal digits of precision(4 bytes)
@@ -19,4 +21,41 @@ int main(){
     float radius = 1.5;
     double volume = (4.0/3.0) * pi * radius * radius * radius;
     printf("The volume of a sphere with radius %f is %f\n", radius, volume);
+
+    // Here I use character variables
+
+    char chaman = 'A';
+
+    printf("The most beautiful letter %c\n", chaman);
+
+    int IsTrue = 1;// Represents true
+    int isFalse = 0;//Represens false
+
+    int age = 25;
+    int isAdult = (age > 18);
+
+    printf("Is Adult? %d\n", isAdult);
+
+    return 0;
+
+    //using <stdbool.h>
+
+    bool isTrue = true;
+    bool isFalse = false;
+
+
+    
+    //Here I'm using Constants
+    #define PI 3.14159;
+
+    const float chilling = 0.14;
+
+    //area of a circle, we're using the constant and the variable radius for this
+
+    float area = (PI * radius * radius);
+
+    printf("The area of this circle is:%d\n",area );
+
+
+
 }
