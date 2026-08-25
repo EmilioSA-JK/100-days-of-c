a3932c0ee01a47405f17b8fdc642abfaadb575a9
EmilioSA-JK <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-24 23:05:48 -0600

    feat: Strings

---
+++ day_1-10/Arrays_&_Strings/strings.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    // A string is considered an array
+    char text[] "Hi nih";
+    int age = 42;
+
+    printf(text);
+
+    
+
+    // The specifier for string is %s
+
+    char name[] = "James";
+    int age = 42;
+
+    printf("%s is %d years old.", name, age);
+
+    return 0;
+}
