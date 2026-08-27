7c8efce24bd7936f6ef94f8c0f1993c6a460e49c
EmilioSA-JK <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-26 20:20:54 -0600

    feat: add fgets function

--- day_1-10/Arrays_&_Strings/strings.c
+++ day_1-10/Arrays_&_Strings/strings.c
@@ -3,19 +3,45 @@
 int main(){
 
     // A string is considered an array
-    char text[] "Hi nih";
+    char text[] = "Hi nih";
     int age = 42;
 
-    printf(text);
+    printf("%s\n" ,text);
 
     
 
     // The specifier for string is %s
 
-    char name[] = "James";
-    int age = 42;
+    //char name[] = "James";
+    //int age = 42;
 
-    printf("%s is %d years old.", name, age);
+    //printf("%s is %d years old.", name, age);
+
+    
+
+    // This is how to take an string input in case is just one string chain(word)
+
+    char name[50];
+    //scanf("%s\n", name);
+
+    //printf("%s\n", name);
+
+   
+
+
+    //If it is needed to take more than one word there's a function called fgets()
+
+
+    /* The three parts are
+    1. The variable that stores the string
+    2. The maximum size of the string
+    3. A keyword to tell the program to take an input from the user
+    */
+
+    fgets(name, 50, stdin);
+
+    printf("Hi %s.", name);
 
     return 0;
+
 }
