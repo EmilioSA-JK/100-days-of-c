62f78a1a911be06e8a84cd0c0780bf765d4b75b9
EmilioSA-JK <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-28 22:08:27 -0600

    feat: length meter function

---
+++ day_1-10/Project-1/passw-analyzer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int lenght(char* passwd[]){
+
+    i = 0;
+    count = 0;
+
+    while (i != '\0') {
+
+        i++;
+        count++;
+
+    }
+
+    return count;
+
+}
+
+
+int main () {
+    
+    
+
+}
